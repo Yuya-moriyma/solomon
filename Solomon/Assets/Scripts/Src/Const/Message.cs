9a73c648a4d8dbb114f7f0b5b77e495b4b71d700
MYCOMPUTER\森山　裕也 <seputember24@gmail.com>
2018-07-17 23:15:26 +0900

    ・タイトル画面モック完成 ・メニュー画面作成

--- Solomon/Assets/Scripts/Src/Const/Message.cs
+++ Solomon/Assets/Scripts/Src/Const/Message.cs
@@ -2,6 +2,12 @@ namespace Const {
     public static class Message {
         //タイトル画面
         public static readonly string TITLE_BUTTON_NEWGAME = "NEW GAME";
-        public static readonly string TITLE_BUTTON_CONTINUE = "CONTINUE"; 
+        public static readonly string TITLE_BUTTON_CONTINUE = "CONTINUE";
+        //メニュー画面
+        public static readonly string MENU_BUTTON_READY = "READY";
+        public static readonly string MENU_BUTTON_SUMMON = "SUMMON";
+        public static readonly string MENU_BUTTON_SHOP = "SHOP";
+        public static readonly string MENU_BUTTON_EDIT = "EDIT";
+        public static readonly string MENU_BUTTON_EXIT = "EXIT";
     }
 }
--- Solomon/Assets/Scripts/Src/View/TitleView.cs
+++ Solomon/Assets/Scripts/Src/View/TitleView.cs
@@ -1,19 +1,25 @@
-﻿
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using Const;
 using UnityEngine;
+using UnityEngine.UI;
 using Util;
 
 public class TitleView : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GameObject button = (GameObject) Resources.Load ("Prefabs/Button/LargeButton");
 		GameObject backGround = (GameObject) GameObject.Find ("BackGround");
-		button = Prefab.instantiate(button, 0, 0) as GameObject;
-		button.transform.SetParent (backGround.transform);
+
+		GameObject _newGame = (GameObject) Resources.Load ("Prefabs/Button/LargeButton");
+		_newGame.GetComponent<Text> ().text = Message.TITLE_BUTTON_NEWGAME;
+		_newGame = Prefab.instantiate (_newGame, 0, -75) as GameObject;
+		_newGame.transform.SetParent (backGround.transform);
+
+		GameObject _continue = (GameObject) Resources.Load ("Prefabs/Button/LargeButton");
+		_continue.GetComponent<Text> ().text = Message.TITLE_BUTTON_CONTINUE;
+		_continue = Prefab.instantiate (_continue, 0, -150) as GameObject;
+		_continue.transform.SetParent (backGround.transform);
 	}
 
 	// Update is called once per frame
