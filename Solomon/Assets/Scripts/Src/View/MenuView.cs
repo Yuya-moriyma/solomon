a516e90b007e311877337ed450d457d13a1faadf
MYCOMPUTER\森山　裕也 <seputember24@gmail.com>
2018-07-19 23:06:06 +0900

    LineRenderer追加

--- Solomon/Assets/Scripts/Src/View/MenuView.cs
+++ Solomon/Assets/Scripts/Src/View/MenuView.cs
@@ -7,13 +7,18 @@ using Util;
 
 public class MenuView : MonoBehaviour {
 
+	LineRenderer renderer;
+
 	// Use this for initialization
 	void Start () {
-		
+		renderer = GameObject.Find ("MenuView").GetComponent<LineRenderer> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 currentPos = renderer.GetPosition (0);
+		if (currentPos.y <= 5.0f) {
+			renderer.SetPosition (0, new Vector3 (currentPos.x, currentPos.y + 0.05f, currentPos.z));
+		}
 	}
 }
